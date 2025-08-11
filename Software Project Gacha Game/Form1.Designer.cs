af5362a6e244c86364ce87a6b3503348eadbddc6
Ian Sandi <ian.tayler.sandi@gmail.com>
2025-08-11 11:09:54 -0600

    Added all folders and base classes.

--- Software Project Gacha Game/Form1.Designer.cs
+++ Software Project Gacha Game/Form1.Designer.cs
@@ -28,18 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            flowLayoutPanel1 = new FlowLayoutPanel();
             SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(12, 12);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1303, 671);
+            flowLayoutPanel1.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1327, 859);
+            ClientSize = new Size(1327, 695);
+            Controls.Add(flowLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
     }
 }
