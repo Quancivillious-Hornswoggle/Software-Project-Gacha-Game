62121ff177c705ae7655bb030f798745d6abb426
Ian Sandi <yellowteeth999@gmail.com>
2025-08-12 00:44:38 -0600

    Slight update to gacha system

--- Software Project Gacha Game/GachaSystem/RewardSplash.cs
+++ Software Project Gacha Game/GachaSystem/RewardSplash.cs
@@ -12,9 +12,13 @@ namespace Software_Project_Gacha_Game.GachaSystem
 {
     public partial class RewardSplash : Form
     {
+        string Reward = string.Empty;
         public RewardSplash(string reward)
         {
             InitializeComponent();
+
+            // Set raw reward 
+            Reward = reward;
 
             // Set open position to center
             this.StartPosition = FormStartPosition.CenterScreen;
@@ -28,6 +32,27 @@ namespace Software_Project_Gacha_Game.GachaSystem
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Optional switch case to physically reward something
+            switch (Reward)
+            {
+                case "Sticker Pack":
+
+                    break;
+                case "Wallpaper":
+
+                    break;
+                case "Ringtone":
+
+                    break;
+                case "Animated Avatar":
+
+                    break;
+                case "Miku Voice Greeting":
+
+                    break;
+
+            }
+
             this.Close();
         }
     }
