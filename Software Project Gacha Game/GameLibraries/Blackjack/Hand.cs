3332f46fee7572b5fbe7511dc048b8fa3ce7e167
Susan Valenzuela <herosuperpuppy@gmail.com>
2025-08-14 15:19:53 -0600

    Finished Updating Hand Class

--- Software Project Gacha Game/GameLibraries/Blackjack/Hand.cs
+++ Software Project Gacha Game/GameLibraries/Blackjack/Hand.cs
@@ -1,12 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Software_Project_Gacha_Game.GameLibraries.Blackjack
 {
-    internal class Hand
+internal class Hand
+{
+    private int cardCount;
+    private Card[] hand;
+
+    public Hand()
     {
+        hand = new Card[12]; // max possible cards in blackjack without busting
+        cardCount = 0;
     }
+
+    public void AddCard(Card card)
+    {
+        if (cardCount < hand.Length)
+        {
+            hand[cardCount++] = card;
+        }
+        else
+        {
+            throw new InvalidOperationException("Hand is full.");
+        }
+    }
+
+    public int GetHandValue()
+    {
+        int totalValue = 0;
+        int aceCount = 0;
+
+        for (int i = 0; i < cardCount; i++)
+        {
+            string rank = hand[i].GetRank();
+            if (rank == "A")
+            {  //count as 11 initially 
+                totalValue += 11;
+                aceCount++;
+            }
+            else if (rank == "K" || rank == "Q" || rank == "J")
+            {
+                totalValue += 10;
+            }
+            else
+            {
+                totalValue += int.Parse(rank);
+            }
+        }
+
+        while (totalValue > 21 && aceCount > 0)
+        { //change to 1
+            totalValue -= 10;
+            aceCount--;
+        }
+
+        return totalValue;
+    }
+
+    public void ClearHand()
+    {
+        for (int i = 0; i < cardCount; i++)
+        {
+            hand[i] = null;
+        }
+        cardCount = 0;
+    }
+
+    public override string ToString()
+    {
+        string output = "";
+        for (int i = 0; i < cardCount; i++)
+        {
+            output += hand[i].ToString() + " ";
+        }
+        return output.Trim();
+    }
+}
 }
